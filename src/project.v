70d7d85d3c32aaa2cc7b7709e918b49a69a44393
HoangDang04 <155784490+HoangDang04@users.noreply.github.com>
2025-09-20 20:16:37 -0400

    Update writing 8-bit counter programmable

--- src/project.v
+++ src/project.v
@@ -6,8 +6,8 @@
 `default_nettype none
 
 module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
+	input  wire [7:0] ui_in,    // Dedicated inputs (this is my load value to get the value of the things)
+	output wire [7:0] uo_out,   // Dedicated outputs (this is my output of the counter value of x amount of clock cycles)
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
@@ -15,13 +15,32 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+	// So this is my counter to save the state of the counter right now
+    reg[7:0] counter;
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+	// There are 3 main signals: load to load the counter, enable to keep counting and dir to check if it is going up or down
+    wire load = uio_in[0];
+    wire enable = uio_in[1];
+    wire dir = uio_in[2];
+
+	// Counter logic
+	always @(posedge clk or negedge rst_n) begin
+		if(!rst_n)
+			counter <= 8'b0;
+		else if (load)
+			counter <= ui_in;	// Load the input value right now
+		else if (enable) begin	// Enable the system
+			if(direction)
+				counter <= counter - 1'b1;
+			else
+				counter <= counter + 1'b1;
+		end
+	end
+
+	assign uo_out = counter;
+	assign uio_oe = {8{enable}};
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+	wire _unused = &{ena, 1'b0};
 
 endmodule
