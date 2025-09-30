ae0d26d44218d8fc1be6e3ed5575a98d2f258f36
HoangDang04 <155784490+HoangDang04@users.noreply.github.com>
2025-09-29 20:38:29 -0400

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -13,18 +13,21 @@ module tb ();
     #1;
   end
 
+   reg clk = 0;
+   always #5 clk = ~clk;   // 100MHz clock
   // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+   
+   reg rst_n;
+   reg ena;
+   reg [7:0] ui_in;
+   reg [7:0] uio_in;
+   wire [7:0] uo_out;
+   wire [7:0] uio_out;
+   wire [7:0] uio_oe;
+   
 `ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
+   wire VPWR = 1'b1;
+   wire VGND = 1'b0;
 `endif
 
   // Replace tt_um_example with your module name:
