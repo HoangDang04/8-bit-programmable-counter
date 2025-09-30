29fe764d52149a75be600921aac3334c396a99ed
HoangDang04 <155784490+HoangDang04@users.noreply.github.com>
2025-09-29 23:08:04 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -4,7 +4,7 @@
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
-
+from cocotb.triggers import Reads only
 
 async def reset_dut(dut, cycles=5):
     """Reset helper (active-low reset)."""
